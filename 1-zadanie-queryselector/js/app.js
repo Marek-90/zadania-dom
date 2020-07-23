1babdfd981e5b7b42a44d446c65484f52f954e87
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-07-23 21:47:36 +0200

    zrobione

--- 1-zadanie-queryselector/js/app.js
+++ 1-zadanie-queryselector/js/app.js
@@ -7,51 +7,51 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let el of nr1) {
         el.classList.add("active")
     }
-
     //------------------   2    --------------------------//
 
-    const nr2 = document.querySelectorAll(".el data-border")
-    console.log(nr2)
+    const nr2 = document.querySelectorAll("[data-border]")
 
+    nr2.forEach((el) => {
+        el.setAttribute("data-el-active", "")
 
+    });
 
     //------------------   3    --------------------------//    <------ ZROBIONE
 
     const nr3 = document.querySelectorAll(".hack")
-    for (let er of nr3) {
-        er.setAttribute("title", "hacking")
+    for (let el of nr3) {
+        el.setAttribute("title", "hacking")
     }
 
 
     //------------------   4    --------------------------//    <------ ZROBIONE
 
     const nr4 = document.querySelectorAll(".hijack")
-    for (let et of nr4) {
-        et.removeAttribute("title")
+    for (let el of nr4) {
+        el.removeAttribute("title")
     }
 
     //------------------   5    --------------------------//    <------ ZROBIONE
 
     const nr5 = document.querySelectorAll(".st1, .st2")
-    for (let ey of nr5) {
-        ey.style.color = ("red")
-        ey.style.fontSizing = ("15px")
+    for (let el of nr5) {
+        el.style.color = ("red")
+        el.style.fontSize = ("15px")
     }
 
     //------------------   6    --------------------------//    <------- ZROBIONE
 
     const nr6 = document.querySelectorAll(".del")
-    for (let ez of nr6) {
-        ez.removeAttribute("data-hack-inactive", "");
-        ez.setAttribute("data-hack-active", "")
+    for (let el of nr6) {
+        el.removeAttribute("data-hack-inactive", "");
+        el.setAttribute("data-hack-active", "")
     }
 
 
     //------------------   7    --------------------------//    <------- ZROBIONE    
 
     const nr7 = document.querySelectorAll(".last-attempt span")
-    console.log(nr7)
-    for (ex of nr7) {
-        ex.remove()
+    for (el of nr7) {
+        el.remove()
     }
 });
